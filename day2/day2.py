bb49cfdd0c0a85f00f76bf84c9b8c353ee385e09
Jenny Zhang <kennynzhang@gmail.com>
2024-12-03 03:36:32 -0800

    day 2 part 2 brute force solution

--- day2/day2.py
+++ day2/day2.py
@@ -2,9 +2,10 @@ import utils
 
 
 def day2part1():
-    input_lines = utils.read_input('../input/day2input.txt')
+    input_lines = utils.read_input('../input/day2input.txt').split()
     safe_lines = 0
-    for line in input_lines:
+    for input_line in input_lines:
+        line = list(int(x) for x in input_line.split())
         if is_safe(line):
             safe_lines += 1
             print(line + ' - safe')
@@ -13,12 +14,28 @@ def day2part1():
     print(safe_lines)
 
 
+def day2part2():
+    input_lines = utils.read_input('../input/day2input.txt')
+    safe_lines = 0
+    for input_line in input_lines:
+        line = list(int(x) for x in input_line.split())
+        for permutation in generate_permutations(line):
+            if is_safe(permutation):
+                safe_lines += 1
+                break
+    print(safe_lines)
+
+
+def generate_permutations(line):
+    for i in range(len(line)):
+        yield line[0:i] + line[i+1:]
+
+
 def is_safe(line):
-    processed_line = list(int(x) for x in line.split())
-    value = processed_line[0]
+    value = line[0]
     is_increasing = None
-    for i in range(1, len(processed_line)):
-        next_value = processed_line[i]
+    for i in range(1, len(line)):
+        next_value = line[i]
         if is_increasing is None:
             is_increasing = next_value > value
         if is_increasing:
@@ -32,4 +49,4 @@ def is_safe(line):
 
 
 if __name__ == "__main__":
-    day2part1()
+    day2part2()
