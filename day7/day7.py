1a3091629b54d00d5130b95dc2ab51fb9244e82f
Jenny Zhang <kennynzhang@gmail.com>
2025-02-17 15:35:20 -0800

    Day 7 part 2

--- day7/day7.py
+++ day7/day7.py
@@ -1,8 +1,8 @@
 # equations = [(totals, [values]), ...]
-def get_valid_total(equations):
+def get_valid_total(equations, validation_method):
     valid_total = 0
     for total, values in equations:
-        if is_valid(total, values):
+        if validation_method(total, values):
             valid_total += total
     return valid_total
 
@@ -13,3 +13,12 @@ def is_valid(total, values):
         new_values_plus = [values[0] + values[1]] + values[2:]
         new_values_times = [values[0] * values[1]] + values[2:]
         return is_valid(total, new_values_plus) or is_valid(total, new_values_times)
+
+def is_valid_with_concat(total, values):
+    if len(values) == 1:
+        return total == values[0]
+    else:
+        new_values_plus = [values[0] + values[1]] + values[2:]
+        new_values_times = [values[0] * values[1]] + values[2:]
+        new_values_concat = [int(str(values[0]) + str(values[1]))] + values[2:]
+        return is_valid_with_concat(total, new_values_plus) or is_valid_with_concat(total, new_values_times) or is_valid_with_concat(total, new_values_concat)
