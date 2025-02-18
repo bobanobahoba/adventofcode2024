fe9f50c960087070c7c5991b28c865fff43f6704
Jenny Zhang <kennynzhang@gmail.com>
2025-02-17 16:19:33 -0800

    Better day7 input handling

--- day7/day7.py
+++ day7/day7.py
@@ -1,5 +1,6 @@
 # equations = [(totals, [values]), ...]
-def get_valid_total(equations, validation_method):
+def get_valid_total(input_lines, validation_method):
+    equations = read_input(input_lines)
     valid_total = 0
     for total, values in equations:
         if validation_method(total, values):
@@ -22,3 +23,12 @@ def is_valid_with_concat(total, values):
         new_values_times = [values[0] * values[1]] + values[2:]
         new_values_concat = [int(str(values[0]) + str(values[1]))] + values[2:]
         return is_valid_with_concat(total, new_values_plus) or is_valid_with_concat(total, new_values_times) or is_valid_with_concat(total, new_values_concat)
+
+def read_input(input_lines):
+    total_arr, values_arr = [], []
+    for line in input_lines:
+        elements = line.strip().replace(':', '').split(' ')
+        total, values = int(elements[0]), [int(elem) for elem in elements[1:]]
+        total_arr.append(total)
+        values_arr.append(values)
+    return zip(total_arr, values_arr)
